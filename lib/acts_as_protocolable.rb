5c9a328158f1b0c8b7ce2fe878ce56c274213cea
Igor Alexandrov <igor.alexandrov@gmail.com>
2010-07-21 17:46:42 +0400

    Fixed renaming from acts_as_protocoled to acts_as_protocolable

--- lib/acts_as_protocolable.rb
+++ lib/acts_as_protocolable.rb
@@ -2,15 +2,12 @@ module ActsAsProtocolable
   mattr_accessor :internal_user_id
 
   module ClassMethods
-    def acts_as_protocoled(options = {})
-      self.acts_as_protocoled_model(options)
+    def acts_as_protocolablable(options = {})
+      self.acts_as_protocolable_model(options)
     end
     
-    def acts_as_protocoled_model(options = {})
-      puts "!!!!!!"
-      puts options
-      puts "!!!!!!"
-      include ActsAsProtocoled::InstanceMethods
+    def acts_as_protocolable_model(options = {})
+      include ActsAsProtocolable::InstanceMethods
       
       unless options.has_key?(:only)
         after_create :protocol_on_create
@@ -27,29 +24,29 @@ module ActsAsProtocolable
   module InstanceMethods
     protected
     def protocol_on_create
-      ActsAsProtocoled::Base.create(
+      ActsAsProtocolable::Base.create(
         :affected_object_type => self.class.to_s,
         :affected_object_id => self.id,
         :action_type => "CREATE",
-        :internal_user_id => ActsAsProtocoled.internal_user_id
+        :internal_user_id => ActsAsProtocolable.internal_user_id
       )
     end
     
     def protocol_on_update
-      ActsAsProtocoled::Base.create(
+      ActsAsProtocolable::Base.create(
         :affected_object_type => self.class.to_s,
         :affected_object_id => self.id,
         :action_type => "UPDATE",
-        :internal_user_id => ActsAsProtocoled.internal_user_id
+        :internal_user_id => ActsAsProtocolable.internal_user_id
       )
     end
     
     def protocol_on_destroy
-      ActsAsProtocoled::Base.create(
+      ActsAsProtocolable::Base.create(
         :affected_object_type => self.class.to_s,
         :affected_object_id => self.id,
         :action_type => "DESTROY",
-        :internal_user_id => ActsAsProtocoled.internal_user_id
+        :internal_user_id => ActsAsProtocolable.internal_user_id
       )
     end
   end
